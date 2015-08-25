559ee7e802a7e3ac1bf84c25665a65a7a234d7ee
Pedro Henrique <system.pedrohenrique@gmail.com>
2015-08-25 10:36:29 -0300

    Remove unsued test code

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -2,18 +2,7 @@
 #include <Helper/Profiler.h>
 #include <Engine/MedievalEngine.h>
 
-#ifdef TEST
-#define CATCH_CONFIG_RUNNER
-#include <ThirdParty/Catch.h>
-#endif
-
 int main(int argc, char** argv) {
-    int resultCode = 0;
-
-    #ifdef TEST
-    resultCode = Catch::Session().run(argc, argv);
-    #endif
-
     ProfileInit();
     ProfileStart();
 
@@ -21,9 +10,9 @@ int main(int argc, char** argv) {
     app.init();
     app.run();
     app.close();
-    resultCode = app.getErrorCode();
 
     ProfileEnd("GameEngine total");
-    return resultCode;
+
+    return app.getErrorCode();
 }
 
