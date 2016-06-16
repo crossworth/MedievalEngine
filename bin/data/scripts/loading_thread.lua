0a4cb8f94472693ea613a5bd9375555b75ddb302
Pedro Henrique <system.pedrohenrique@gmail.com>
2016-06-16 12:50:32 -0300

    Loading Thread

---
+++ bin/data/scripts/loading_thread.lua
@@ -0,0 +1,14 @@
+-- MedievalEngine
+-- You can set every file that you will be using on the game here
+-- it will be loaded at the loading screen
+
+-- Call engine_set_loading_done(true) on the end of the file or after done loading everything
+
+function load()
+    load_image("background_loading_screen.png")
+
+
+    -- engine_set_loading_done(true)
+end
+
+
