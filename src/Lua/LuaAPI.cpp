5431a857e3c5f87dec4b3e12fa9448071ce9f999
Pedro Henrique <system.pedrohenrique@gmail.com>
2016-06-16 12:49:13 -0300

    unload functions

--- src/Lua/LuaAPI.cpp
+++ src/Lua/LuaAPI.cpp
@@ -4,10 +4,28 @@ using namespace ME;
 
 sol::state LuaAPI::state;
 
+std::thread LuaAPI::mScriptThread;
+std::mutex LuaAPI::mScritpThreadLock;
+
 bool LuaAPI::mInitialized = false;
 
 LuaAPI::LuaAPI() {
 
+}
+
+void LuaAPI::unloadFunctions(std::vector<std::string> &functions) {
+    std::string unload_command = "";
+
+    std::vector<std::string>::iterator it = functions.begin();
+    for(; it != functions.end(); it++) {
+        unload_command += *it + " = nil ";
+    }
+
+    try {
+        LuaAPI::state.script(unload_command);
+    } catch(sol::error& err) {
+        LOG << Log::LUA_WARNING << err.what() << std::endl;
+    }
 }
 
 void LuaAPI::loadLibs() {
@@ -16,11 +34,10 @@ void LuaAPI::loadLibs() {
         LuaAPI::state.open_libraries(sol::lib::base, sol::lib::string, sol::lib::table, sol::lib::math, sol::lib::bit32, sol::lib::coroutine);
 
         // print text with no debug level (clean print)
-        LuaAPI::state.set_function("print_text", [](const std::string& message){
+        LuaAPI::state.set_function("print_text", [](const std::string& message) {
             LOG << message << std::endl;
         });
         LuaExportAPI::exports("print_text", "string", "void", LuaExportType::FUNCTION, "abstract print string to console");
-
 
         // inject a custom lua print table function verbose and non verbose
         LuaAPI::state.script("function print_table_verbose (tbl, indent) if not indent then indent = 0 end for k, v in pairs(tbl) do formatting = string.rep(\"  \", indent) .. k .. \": \" if type(v) == \"table\" then log(formatting) print_table_verbose(v, indent+1) elseif type(v) == 'boolean' then log(formatting .. tostring(v)) else log(formatting .. v) end end end");
@@ -31,17 +48,17 @@ void LuaAPI::loadLibs() {
         LuaExportAPI::exports("print_table", "table[, int]", "void", LuaExportType::FUNCTION, "print a lua table on console, second argument control if it should have indentation or not, pass a int as the indentation number");
 
 
-        LuaAPI::state.set_function("log_v", [](const std::string& message){
+        LuaAPI::state.set_function("log_v", [](const std::string& message) {
             LOG << Log::LUA_VERBOSE << message << std::endl;
         });
         LuaExportAPI::exports("log_v", "string", "void", LuaExportType::FUNCTION, "log a verbose message to console");
 
-        LuaAPI::state.set_function("log_w", [](const std::string& message){
+        LuaAPI::state.set_function("log_w", [](const std::string& message) {
             LOG << Log::LUA_WARNING << message << std::endl;
         });
         LuaExportAPI::exports("log_w", "string", "void", LuaExportType::FUNCTION, "log a warning message to console");
 
-        LuaAPI::state.set_function("log_c", [](const std::string& message){
+        LuaAPI::state.set_function("log_c", [](const std::string& message) {
             LOG << Log::LUA_CRITICAL << message << std::endl;
         });
         LuaExportAPI::exports("log_c", "string", "void", LuaExportType::FUNCTION, "log a critical message to console,");
@@ -54,7 +71,7 @@ void LuaAPI::loadLibs() {
         LuaAPI::state.script("function log(something) if (type(something) == \"table\") then print_table_verbose(something) else log_v(tostring(something)) end end");
         LuaExportAPI::exports("log", "mixed", "void", LuaExportType::FUNCTION, "log a value on console, argument could be table, variable, string, bool, integer, float");
 
-        LuaAPI::state.set_function("register_function", [](const std::string& name){
+        LuaAPI::state.set_function("register_function", [](const std::string& name) {
             LuaExportAPI::exports(name, "", "", LuaExportType::FUNCTION, "LuaAPI definied function");
         });
         LuaExportAPI::exports("register_function", "string", "void", LuaExportType::FUNCTION, "register an function on the LuaExportAPI for the console Prediction list, pass the function name as argument");
@@ -70,7 +87,9 @@ void LuaAPI::script(const std::string &code) {
         return;
     }
 
-    std::thread scriptThread([code] () {
+    mScritpThreadLock.lock();
+    
+    mScriptThread = std::thread([code] () {
         try {
             sol::thread runner = sol::thread::create(LuaAPI::state.lua_state());
             runner.state().script(code);
@@ -78,55 +97,22 @@ void LuaAPI::script(const std::string &code) {
             LOG << Log::LUA_WARNING << err.what() << std::endl;
         }
     });
-    scriptThread.detach();
+
+    mScriptThread.detach();
+    mScritpThreadLock.unlock();
 }
 
-void LuaAPI::executeScript(const std::string &fileName) {
+bool LuaAPI::executeScript(const std::string &fileName) {
     if (!LuaAPI::mInitialized) {
-        return;
+        return false;
     }
 
-    std::thread executeThread([fileName] () {
-        try {
-            sol::thread runner = sol::thread::create(LuaAPI::state.lua_state());
-            runner.state().script_file(ENGINE_DEFAULTS::SCRIPTS_PATH + fileName);
-        } catch(sol::error& err) {
-            LOG << Log::LUA_WARNING << err.what() << std::endl;
-        }
-    });
-    executeThread.detach();
+    std::string code = Script::load(fileName);
+
+    if (code != "") {
+        LuaAPI::script(code);
+        return true;
+    }
+
+    return false;
 }
-
-void LuaAPI::scriptSync(const std::string &code) {
-    if (!LuaAPI::mInitialized) {
-        return;
-    }
-
-    try {
-        LuaAPI::state.script(code);
-    } catch(sol::error& err) {
-        LOG << Log::LUA_WARNING << err.what() << std::endl;
-    }
-}
-
-void LuaAPI::removeFunctions(const std::vector<std::string> &functions) {
-    for (auto it = functions.begin(); it != functions.end(); it++) {
-        std::cout << "Func:" << *it << std::endl;
-        LuaAPI::state.set_function(*it, [] () {
-
-        });
-    }
-}
-
-void LuaAPI::executeScriptSync(const std::string &fileName) {
-    if (!LuaAPI::mInitialized) {
-        return;
-    }
-
-    try {
-        LuaAPI::state.script_file(ENGINE_DEFAULTS::SCRIPTS_PATH + fileName);
-    } catch(sol::error& err) {
-        LOG << Log::LUA_WARNING << err.what() << std::endl;
-    }
-}
-
