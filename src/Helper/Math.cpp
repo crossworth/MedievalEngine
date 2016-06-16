40d058b2d2abf58cd2a7d3c3135675abe93d8de2
Pedro Henrique <system.pedrohenrique@gmail.com>
2016-06-16 14:04:35 -0300

    New Math class

---
+++ src/Helper/Math.cpp
@@ -0,0 +1,17 @@
+/*
+* @Project MedievalEngine
+* @Author: Pedro Henrique - system.pedrohenrique@gmail.com
+* @Website: http://pedrohenrique.ninja
+* @Date:   2016-06-16 13:54:57
+*
+*
+* @File: Math.cpp
+* @Last Modified by:   Pedro Henrique
+* @Last Modified time: 2016-06-16 13:57:13
+*/
+
+#include "Math.h"
+
+using namespace ME;
+
+const float Math::PI = 3.141592654f;
---
+++ src/Helper/Math.h
@@ -0,0 +1,16 @@
+#ifndef MEDIEVALENGINE_HELPER_MATH_H
+#define MEDIEVALENGINE_HELPER_MATH_H
+
+#include "Helper/BasicTypes.h"
+
+namespace ME {
+
+class Math {
+public:
+	static const float PI;
+
+};
+
+}
+
+#endif // MEDIEVALENGINE_HELPER_MATH_H
